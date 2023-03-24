218be5e78a2f486bf20c8acdd1e40d7943a601f8
Paul Valdez <18030566@itcelaya.edu.mx>
2023-03-23 23:08:55 -0600

    Inicio Para la creación de Laravel y Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel(
+            {
+                input: [
+                    'resources/css/app.css', 'resources/js/app.js'
+                ],
+                refresh: true,
+                // valetTls: 'my-app.test'
+            }
+        ),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js'
+        }
+    }
 });
